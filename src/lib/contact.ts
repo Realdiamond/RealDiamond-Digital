b99dd37231cdc5fc2b1da4f6f5dc2605e32fbd7d
Realdiamond <akinsanmioluwatimilehin@gmail.com>
2026-03-19 13:02:23 +0100

    Fix contact HTML escape lint issue

--- src/lib/contact.ts
+++ src/lib/contact.ts
@@ -29,6 +29,6 @@ export function escapeHtml(value: string): string {
     .replace(/&/g, "&amp;")
     .replace(/</g, "&lt;")
     .replace(/>/g, "&gt;")
-    .replace(/\"/g, "&quot;")
+    .replace(/"/g, "&quot;")
     .replace(/'/g, "&#39;");
 }
